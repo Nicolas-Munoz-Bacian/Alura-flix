f4e6d46fe30a00b9eb838cb9f951c4b904612de1
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-11 19:44:56 -0300

    Add files via upload

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,25 +1,25 @@
-import Banner from "components/Banner";
-import styles from "./Favoritos.module.css";
-import Titulo from "components/Titulo";
-import Card from "components/Card";
-import { useFavoritosContext } from "context/Favoritos";
+import Banner from "../../components/Banner"
+import styles from "../Favoritos/Favoritos.module.css"
+import Titulo from "../../components/Titulo";
+import Card from "../../components/Card";
+import { useFavoritosContext } from "../Context/Favoritos";
 
-function Favoritos() {
-  const { favorito } = useFavoritosContext();
+function Favoritos(){
+  const {favorito} = useFavoritosContext()
 
-  return (
-    <>
-      <Banner img="favorite" color="#44d97d" />
-      <Titulo>
-        <h1>Mis favoritos</h1>
-      </Titulo>
-      <section className={styles.container}>
-        {favorito.map((fav) => {
-          return <Card {...fav} key={fav.id} />;
-        })}
-      </section>
-    </>
-  );
-}
+  return <>
+  <Banner img="favorite" color="#AF7EAA"/>
+  <Titulo>
+    <h1>Mis Favoritos</h1>
+  </Titulo>
+  <section className={styles.container}>
+    {favorito.map(fav=>{
 
-export default Favoritos;
+      return <Card {...fav} key={fav.id} />
+
+    })}
+  </section>
+  </>;
+  }
+
+  export default Favoritos;
