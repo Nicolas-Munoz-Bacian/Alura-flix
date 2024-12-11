d80146113a1a352d8dcb9757e2e2c2bf9cde1c75
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-11 19:34:21 -0300

    Add files via upload

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,28 +1,37 @@
-import Banner from "../../components/Banner"
-import Titulo from "../../components/Titulo"
+import Banner from "../../components/Banner";
+import Titulo from "../../components/Titulo";
 import Card from "../../components/Card";
-import styles from "../../pages/inicio/index.modules.css"
-import videos from "../../components/data/db.json"
+import styles from "../inicio/index.module.css";
+import { useState, useEffect } from "react";
 
-function inicio(){
-    return(
-        <>
+function Inicio() {
+  const [videos, setVideos] = useState([]);
 
-                <Banner img="./img/eliminar" color="#154580"/>
-                <Titulo>
-                    <h1>Un lugar para guardar sus videos favoritos de Alura Latam</h1>
-                </Titulo>
-                <Card id="1" titulo="Equipo Front End" capa="https://youtu.be/rpvrLaBQwgg?si=UmyErXE1Xc_WYR0D"/>
-                
-                <section className={styles.container}>
-                    {videos.map((video)=>{
-                        
-                        return <Card {...videos} key={video.id}/>
-                    })}
-                </section>
-        </>
-
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/DaniRiverol/alura-cinema-api/videos"
     )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+      });
+  }, []);
+  return(
+    <>
+            <Banner img="./img/eliminar" color="#154580"/>
+            <Titulo>
+                <h1>Un lugar para guardar sus videos favoritos de Alura Latam</h1>
+            </Titulo>
+            <Card id="1" titulo="Equipo Front End" capa="https://youtu.be/rpvrLaBQwgg?si=UmyErXE1Xc_WYR0D"/>
+            
+            <section className={styles.container}>
+                {videos.map((video)=>{
+                    
+                    return <Card {...videos} key={video.id}/>
+                })}
+            </section>
+    </>
+  );
 }
 
-export default inicio;
+export default Inicio;
