445bdc30de5b8039d306829e1bede3fd8455aad1
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-11 21:03:55 -0300

    Add files via upload

--- src/components/Pie/index.js
+++ src/components/Pie/index.js
@@ -1,8 +1,9 @@
 import styles from './Pie.module.css'
 function Pie() {
     return (
-        <footer className={styles.rodape}>//modificar
+        <footer className={styles.rodape}>
             <h2>Desarrollado por Nicolás Muñoz Bacián ©Diseñador Gráfico.</h2>
+
             
         </footer>
     )
